e429e59cf304ebfe8e5e5f53f580119675bc63aa
Gurshawn Lehal <shawn10067@gmail.com>
2022-04-01 18:42:50 -0400

    main

--- part9/testapp/src/App.tsx
+++ part9/testapp/src/App.tsx
@@ -1,21 +1,54 @@
 const App = () => {
-  const courseName = "Half Stack application development";
-  interface CourseAttributes {
+  // new types
+  interface CoursePartBase {
     name: string;
     exerciseCount: number;
+    type: string;
   }
-  const courseParts: Array<CourseAttributes> = [
+
+  interface CourseNormalPart extends CoursePartBase {
+    type: "normal";
+    description: string;
+  }
+  interface CourseProjectPart extends CoursePartBase {
+    type: "groupProject";
+    groupProjectCount: number;
+  }
+
+  interface CourseSubmissionPart extends CoursePartBase {
+    type: "submission";
+    description: string;
+    exerciseSubmissionLink: string;
+  }
+
+  type CoursePart = CourseNormalPart | CourseProjectPart | CourseSubmissionPart;
+
+  // this is the new coursePart variable
+  const courseParts: CoursePart[] = [
     {
       name: "Fundamentals",
       exerciseCount: 10,
+      description: "This is the leisured course part",
+      type: "normal",
+    },
+    {
+      name: "Advanced",
+      exerciseCount: 7,
+      description: "This is the harded course part",
+      type: "normal",
     },
     {
       name: "Using props to pass data",
       exerciseCount: 7,
+      groupProjectCount: 3,
+      type: "groupProject",
     },
     {
       name: "Deeper type usage",
       exerciseCount: 14,
+      description: "Confusing description",
+      exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev",
+      type: "submission",
     },
   ];
 
