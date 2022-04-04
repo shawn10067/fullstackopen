a1c0cb84c371538a0804488b27f1dd16100bba31
shawn10067 <shawn10067@gmail.com>
2022-04-04 00:34:06 -0400

    modified:   part9/testapp/src/App.tsx

--- part9/testapp/src/App.tsx
+++ part9/testapp/src/App.tsx
@@ -1,3 +1,9 @@
+const assertNever = (value: never): never => {
+  throw new Error(
+    `Unhandled discriminated union member: ${JSON.stringify(value)}`
+  );
+};
+
 const App = () => {
   // new types
   interface CoursePartBase {
@@ -6,22 +12,33 @@ const App = () => {
     type: string;
   }
 
-  interface CourseNormalPart extends CoursePartBase {
-    type: "normal";
+  interface CoursePartDescription extends CoursePartBase {
     description: string;
+  }
+
+  interface CourseNormalPart extends CoursePartDescription {
+    type: "normal";
   }
   interface CourseProjectPart extends CoursePartBase {
     type: "groupProject";
     groupProjectCount: number;
   }
 
-  interface CourseSubmissionPart extends CoursePartBase {
+  interface CourseSubmissionPart extends CoursePartDescription {
     type: "submission";
-    description: string;
     exerciseSubmissionLink: string;
   }
 
-  type CoursePart = CourseNormalPart | CourseProjectPart | CourseSubmissionPart;
+  interface CourseRequirementPart extends CoursePartDescription {
+    type: "special";
+    requirements: Array<string>;
+  }
+
+  type CoursePart =
+    | CourseNormalPart
+    | CourseProjectPart
+    | CourseSubmissionPart
+    | CourseRequirementPart;
 
   // this is the new coursePart variable
   const courseParts: CoursePart[] = [
@@ -50,34 +67,82 @@ const App = () => {
       exerciseSubmissionLink: "https://fake-exercise-submit.made-up-url.dev",
       type: "submission",
     },
+    {
+      name: "Backend development",
+      exerciseCount: 21,
+      description: "Typing the backend",
+      requirements: ["nodejs", "jest"],
+      type: "special",
+    },
   ];
 
   // components
-
   // header
   const Header = ({ title }: { title: string }) => {
     return <h2>{title}</h2>;
   };
 
-  //  content
-  const Content = ({ courses }: { courses: Array<CoursePartBase> }) => {
+  // part
+  const Part = ({ element }: { element: CoursePart }): JSX.Element => {
+    switch (element.type) {
+      case "normal":
+        return (
+          <p key={element.name}>
+            {element.name}
+            <ul>
+              <li>{element.exerciseCount}</li>
+              <li>{element.description}</li>
+            </ul>
+          </p>
+        );
+      case "groupProject":
+        return (
+          <p key={element.name}>
+            {element.name}
+            <ul>
+              <li>{element.exerciseCount}</li>
+              <li>{element.groupProjectCount}</li>
+            </ul>
+          </p>
+        );
+      case "submission":
+        return (
+          <p key={element.name}>
+            {element.name}
+            <ul>
+              <li>{element.exerciseCount}</li>
+              <li>{element.description}</li>
+              <li>{`Submittion link: ${element.exerciseSubmissionLink}`}</li>
+            </ul>
+          </p>
+        );
+      case "special":
+        return (
+          <p key={element.name}>
+            {element.name}
+            <ul>
+              <li>{element.exerciseCount}</li>
+              <li>{element.description}</li>
+              <li>{`Requirements: ${element.requirements.join(", ")}`}</li>
+            </ul>
+          </p>
+        );
+      default:
+        return assertNever(element);
+    }
+  };
+
+  // content
+  const Content = ({ courses }: { courses: Array<CoursePart> }) => {
     const mappedCourses = courses.map((val) => {
-      return (
-        <p>
-          {val.name} {val.exerciseCount}
-        </p>
-      );
+      return <Part element={val} />;
     });
     console.log(mappedCourses);
-    return (
-      <div>
-        <p>{mappedCourses}</p>
-      </div>
-    );
+    return <div>{mappedCourses}</div>;
   };
 
   // total
-  const Total = ({ courses }: { courses: Array<CoursePartBase> }) => {
+  const Total = ({ courses }: { courses: Array<CoursePart> }) => {
     return (
       <p>
         Number of exercises{" "}
