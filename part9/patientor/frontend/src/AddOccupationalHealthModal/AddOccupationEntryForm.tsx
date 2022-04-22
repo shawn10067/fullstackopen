5de69ac4a5ef5d00f9c23e662cc253f1042bb8ec
GurshawnL <58784041+GurshawnL@users.noreply.github.com>
2022-04-22 00:00:52 -0400

    main

--- part9/patientor/frontend/src/AddOccupationalHealthModal/AddOccupationEntryForm.tsx
+++ part9/patientor/frontend/src/AddOccupationalHealthModal/AddOccupationEntryForm.tsx
@@ -2,25 +2,24 @@ import React from "react";
 import { Grid, Button } from "@material-ui/core";
 import { Field, Formik, Form } from "formik";
 
-import {
-  TextField,
-  DiagnosisSelection,
-  NumberField,
-} from "../AddPatientModal/FormField";
-import { HealthCheckEntry } from "../types";
+import { TextField, DiagnosisSelection } from "../AddPatientModal/FormField";
+import { OccupationalHealthcareEntry } from "../types";
 import { useStateValue } from "../state";
 
 /*
  * use type Health, but omit id and entries,
  */
-export type HealthEntryFormValues = Omit<HealthCheckEntry, "id" | "type">;
+export type OccupationalEntryFormValues = Omit<
+  OccupationalHealthcareEntry,
+  "id" | "type"
+>;
 
 interface Props {
-  onSubmit: (values: HealthEntryFormValues) => void;
+  onSubmit: (values: OccupationalEntryFormValues) => void;
   onCancel: () => void;
 }
 
-export const AddEntryForm = ({ onSubmit, onCancel }: Props) => {
+export const AddOccupationEntryForm = ({ onSubmit, onCancel }: Props) => {
   const [{ diagnoses }] = useStateValue();
 
   return (
@@ -30,7 +29,11 @@ export const AddEntryForm = ({ onSubmit, onCancel }: Props) => {
         date: "",
         specialist: "",
         diagnosisCodes: [],
-        healthCheckRating: 0,
+        employerName: "",
+        sickLeave: {
+          startDate: "",
+          endDate: "",
+        },
       }}
       onSubmit={onSubmit}
       validate={(values) => {
@@ -42,8 +45,11 @@ export const AddEntryForm = ({ onSubmit, onCancel }: Props) => {
         if (!values.description) {
           errors.description = requiredError;
         }
-        if (!values.healthCheckRating) {
-          errors.healthCheckRating = requiredError;
+        if (!values.employerName) {
+          errors.employerName = requiredError;
+        }
+        if (!values.sickLeave) {
+          errors.sickLeave = requiredError;
         }
         if (!values.specialist) {
           errors.specialist = requiredError;
@@ -73,11 +79,22 @@ export const AddEntryForm = ({ onSubmit, onCancel }: Props) => {
               component={TextField}
             />
             <Field
-              label="Health Check Ratings"
-              min={0}
-              max={4}
-              name="healthCheckRating"
-              component={NumberField}
+              label="Employer Name"
+              placeholder="Employer"
+              name="employerName"
+              component={TextField}
+            />
+            <Field
+              label="Sick Leave Start Date"
+              placeholder="YYYY-MM-DD"
+              name="sickLeave.startDate"
+              component={TextField}
+            />
+            <Field
+              label="Sick Leave End Date"
+              placeholder="YYYY-MM-DD"
+              name="sickLeave.endDate"
+              component={TextField}
             />
             <DiagnosisSelection
               diagnoses={Object.values(diagnoses)}
@@ -116,4 +133,4 @@ export const AddEntryForm = ({ onSubmit, onCancel }: Props) => {
   );
 };
 
-export default AddEntryForm;
+export default AddOccupationEntryForm;
--- part9/patientor/frontend/src/PatientListPage/SinglePage.tsx
+++ part9/patientor/frontend/src/PatientListPage/SinglePage.tsx
@@ -14,6 +14,8 @@ import AddHealthEntryModal from "../AddHealthModal";
 import { Button } from "@material-ui/core";
 import { HospitalEntryFormValues } from "../AddEntryModal/AddHealthEntryForm";
 import { HealthEntryFormValues } from "../AddHealthModal/AddEntryForm";
+import AddOccupationEntryModal from "../AddOccupationalHealthModal";
+import { OccupationalEntryFormValues } from "../AddOccupationalHealthModal/AddOccupationEntryForm";
 
 const Person = (): JSX.Element => {
   // getting the id of the patient
@@ -52,9 +54,9 @@ const Person = (): JSX.Element => {
     setHealthError(undefined);
   };
 
-  /*
-      // holding modal state (occupation)
-  const [occupationModalOpen, setOccupationModalOpen] = React.useState<boolean>(false);
+  // holding modal state (occupation)
+  const [occupationModalOpen, setOccupationModalOpen] =
+    React.useState<boolean>(false);
   const [occupationError, setOccupationError] = React.useState<string>();
 
   const openOccupationModal = (): void => setOccupationModalOpen(true);
@@ -63,7 +65,6 @@ const Person = (): JSX.Element => {
     setOccupationModalOpen(false);
     setOccupationError(undefined);
   };
-  */
 
   // defining function to submit new entry
   const submitNewHospitalEntry = async (values: HospitalEntryFormValues) => {
@@ -102,7 +103,7 @@ const Person = (): JSX.Element => {
     console.log("Health submitted", values);
     const entry = {
       ...values,
-      type: "Health",
+      type: "HealthCheck",
       diagnosisCodes: [],
     };
     try {
@@ -115,7 +116,7 @@ const Person = (): JSX.Element => {
       } else {
         setEntries([newEntry]);
       }
-      closeModal();
+      closeHealthModal();
     } catch (e: unknown) {
       if (axios.isAxiosError(e)) {
         console.error(e?.response?.data || "Unrecognized axios error");
@@ -129,39 +130,39 @@ const Person = (): JSX.Element => {
     }
   };
 
-  /*
-    // defining function to submit new entry
-    const submitNewOccupationalEntry = async (values: HealthEntryFormValues) => {
-      console.log("Health submitted", values);
-      const entry = {
-        ...values,
-        type: "Health",
-        diagnosisCodes: [],
-      };
-      try {
-        const { data: newEntry } = await axios.post<Entry>(
-          `${apiBaseUrl}/patients/id/${id || ""}/entries`,
-          entry
-        );
-        if (person.entries !== undefined) {
-          setEntries(entries.concat(newEntry));
-        } else {
-          setEntries([newEntry]);
-        }
-        closeModal();
-      } catch (e: unknown) {
-        if (axios.isAxiosError(e)) {
-          console.error(e?.response?.data || "Unrecognized axios error");
-          setError(
-            String(e?.response?.data?.error) || "Unrecognized axios error"
-          );
-        } else {
-          console.error("Unknown error", e);
-          setError("Unknown error");
-        }
-      }
+  // defining function to submit new entry
+  const submitNewOccupationalEntry = async (
+    values: OccupationalEntryFormValues
+  ) => {
+    console.log("Occupation submitted", values);
+    const entry = {
+      ...values,
+      type: "OccupationalHealthcare",
+      diagnosisCodes: [],
     };
-    */
+    try {
+      const { data: newEntry } = await axios.post<Entry>(
+        `${apiBaseUrl}/patients/id/${id || ""}/entries`,
+        entry
+      );
+      if (person.entries !== undefined) {
+        setEntries(entries.concat(newEntry));
+      } else {
+        setEntries([newEntry]);
+      }
+      closeOccupationModal();
+    } catch (e: unknown) {
+      if (axios.isAxiosError(e)) {
+        console.error(e?.response?.data || "Unrecognized axios error");
+        setError(
+          String(e?.response?.data?.error) || "Unrecognized axios error"
+        );
+      } else {
+        console.error("Unknown error", e);
+        setError("Unknown error");
+      }
+    }
+  };
 
   // getting the patient data
   useEffect(() => {
@@ -307,11 +308,20 @@ const Person = (): JSX.Element => {
         error={healthError}
         onClose={closeHealthModal}
       />
+      <AddOccupationEntryModal
+        modalOpen={occupationModalOpen}
+        onSubmit={submitNewOccupationalEntry}
+        error={occupationError}
+        onClose={closeOccupationModal}
+      />
       <Button variant="contained" onClick={() => openModal()}>
         Add New Hospital Entry
       </Button>
       <Button variant="contained" onClick={() => openHealthModal()}>
         Add New Health Entry
+      </Button>
+      <Button variant="contained" onClick={() => openOccupationModal()}>
+        Add New Occupation Entry
       </Button>
     </div>
   );
