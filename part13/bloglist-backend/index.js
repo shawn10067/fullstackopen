ea4c30bb499ee561bc7e0db615b49adac3a0f8e1
shawn10067 <shawn10067@gmail.com>
2022-05-25 20:08:56 -0400

    main

--- part13/bloglist-backend/index.js
+++ part13/bloglist-backend/index.js
@@ -1,6 +1,12 @@
 const Blog = require("./Models/blog");
 const sequelize = require("./utils/sequelize");
+const express = require("express");
+const blogRouter = require("./Controllers/blog");
+const app = express();
 
+app.use("/blogs", blogRouter);
+
+/*
 const main = async () => {
   try {
     // connecting
@@ -19,5 +25,11 @@ const main = async () => {
     console.log("Error connecting", error);
   }
 };
-
 main();
+*/
+
+const PORT = process.env.PORT || 3001;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
